2f4de553d6b0bfac4123f037097bd44b531e1311
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-07-23 00:13:35 -0400

    created projects array

--- .history/src/components/Nav/index_20220723000205.js
+++ .history/src/components/Nav/index_20220723000205.js
@@ -1,22 +1,6 @@
 import React from 'react';
 
 function Nav() {
-    const projects = [
-        {
-            name: "recipez",
-            description:
-            "A recipt generator built using Javascript, Bulma, and twi APIs: Spoontacular and The Cocktail DB"
-        },
-        {
-            name: "codingquiz",
-            description: "A simple application that showcases vanilla Javascript, HTML, and CSS"
-        },
-        {
-            name: "socialnetwork",
-            description: "Using MongoDB and Express.js, created a social network API that allows users to share their thoughts, react to friends' thoughts, and create a friend list."
-        }
-    ]
-
     return(
         <header>
             <h2>
